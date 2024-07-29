1359d15af04151dd26cdd782d3dc2e20c4eca6b3
kevinhuanca <kevinhuanca75@gmail.com>
2024-07-29 20:44:27 -0300

    Update professional mvc

--- controllers/profesional.js
+++ controllers/profesional.js
@@ -1,4 +1,5 @@
 import { ProfesionalModel } from "../models/profesional.js";
+import { UsuarioModel } from "../models/usuario.js";
 import bcrypt from 'bcrypt';
 
 export class ProfesionalController {
@@ -8,24 +9,142 @@ export class ProfesionalController {
       if (req.session.rol === 2) res.redirect('/prescripciones');
       if (req.session.rol === 1) {
          const profesionales = await ProfesionalModel.obtenerProfesionales();
+
+         if (profesionales.length > 0) {
+            for (const profesional of profesionales) {
+               const profesion = await ProfesionalModel.obtenerProfesionPorId(profesional.id_profesion);
+               profesional.profesion = profesion;
+               delete profesional.id_profesion;
+               
+               const especialidades = await ProfesionalModel.obtenerEspecialidadesPorProfesional(profesional.id);
+               if (especialidades.length > 0) {
+                  for (const especialidad of especialidades) {
+                     const tablaEspecialidad = await ProfesionalModel.obtenerEspecialidadPorId(especialidad.id_especialidad);
+                     especialidad.id = tablaEspecialidad.id;
+                     especialidad.nombre = tablaEspecialidad.nombre;
+                     delete especialidad.id_profesional;
+                     delete especialidad.id_especialidad;
+                  }
+                  profesional.especialidades = especialidades;
+               } else { profesional.especialidades = null; }
+            }  
+         } else { profesionales = null; }
+
          const profesiones = await ProfesionalModel.obtenerProfesiones();
-         res.render('profesional', { profesionales, profesiones });
+         const especialidades = await ProfesionalModel.obtenerEspecialidades();
+         res.render('profesional', { profesionales, profesiones, especialidades, session: req.session });
       }
    }
 
-   static async obtenerEspecialidadesPorProfesion(req, res) { // 🟢
-      const idProfesion = req.params.idProfesion;
-      const especialidades = await ProfesionalModel.obtenerEspecialidadesPorProfesion(idProfesion);
-      res.json(especialidades);
-   }
-
    static async agregarProfesional(req, res) { // 🟢
-      const { nombre, apellido, documento, domicilio, idEspecialidad, matricula, refeps, idUsuario } = req.body;
-      const nuevoProfesional = await ProfesionalModel.agregarProfesional(nombre, apellido, documento, domicilio, matricula, refeps, 1, idEspecialidad, idUsuario);
-      if (nuevoProfesional) {
-         res.send({ message: 'Profesional agregado exitosamente', idProfesional: nuevoProfesional });
+      const { nombre, apellido, documento, domicilio, idProfesion, idEspecialidades, matricula, refeps, idUsuario } = req.body;
+      const idProfesional = await ProfesionalModel.agregarProfesional(nombre, apellido, documento, domicilio, matricula, refeps, 1, idProfesion, idUsuario);
+      
+      if (idProfesional) {
+         if (idEspecialidades) {
+            for (const idEspecialidad of idEspecialidades) {
+               await ProfesionalModel.agregarProfesionalEspecialidad(idProfesional, idEspecialidad);
+            }
+         }
+         res.send({ message: 'Profesional agregado exitosamente', idProfesional: idProfesional });
       } else {
          res.status(500).json({ message: 'Error al registrar el profesional' });
+      }
+   }
+
+   static async obtenerProfesionalPorId(req, res) { // 🟢
+      const { id } = req.params;
+      const profesional = await ProfesionalModel.obtenerProfesionalPorId(id);
+
+      if (profesional) {
+         const profesion = await ProfesionalModel.obtenerProfesionPorId(profesional.id_profesion);
+         profesional.profesion = profesion;
+         delete profesional.id_profesion;
+
+         const especialidades = await ProfesionalModel.obtenerEspecialidadesPorProfesional(id);
+         if (especialidades.length > 0) {
+            for (const especialidad of especialidades) {
+               const tablaEspecialidad = await ProfesionalModel.obtenerEspecialidadPorId(especialidad.id_especialidad);
+               especialidad.id = tablaEspecialidad.id;
+               especialidad.nombre = tablaEspecialidad.nombre;
+               delete especialidad.id_profesional;
+               delete especialidad.id_especialidad;
+            }
+            profesional.especialidades = especialidades;
+         } else { profesional.especialidades = null; }
+
+         res.json(profesional);
+      } else {
+         res.status(404).json({ message: 'Profesional no encontrado' });
+      }
+   }
+
+   static async editarProfesional(req, res) { // 🟢
+      const { id, nombre, apellido, documento, domicilio, idProfesion, idEspecialidades, matricula, refeps} = req.body;
+      const actualizado = await ProfesionalModel.editarProfesional(id, nombre, apellido, documento, domicilio, matricula, refeps, idProfesion);
+      if (actualizado) {
+         if (idEspecialidades) {
+            await ProfesionalModel.eliminarProfesionalEspecialidad(id);
+            for (const idEspecialidad of idEspecialidades) {
+               await ProfesionalModel.agregarProfesionalEspecialidad(id, idEspecialidad);
+            }
+         } else { await ProfesionalModel.eliminarProfesionalEspecialidad(id); }
+
+         const profesional = await ProfesionalModel.obtenerProfesionalPorId(id);
+         if (profesional.documento !== documento) {
+            const claveHasheada = await bcrypt.hash(documento, 10);
+            await UsuarioModel.actualizarUsuario(profesional.id_usuario, documento, claveHasheada);
+         }
+
+         res.send({ message: 'Profesional actualizado exitosamente' });
+      } else {
+         res.status(500).json({ message: 'Error al actualizar el profesional' });
+      }
+   }
+
+   static async habilitarProfesional(req, res) { // 🟢
+      const { id } = req.body;
+      const habilitado = await ProfesionalModel.habilitarProfesional(id);
+      if (habilitado) {
+         const profesional = await ProfesionalModel.obtenerProfesionalPorId(id);
+         const usuario = await UsuarioModel.obtenerUsuarioPorId(profesional.id_usuario);
+         await UsuarioModel.habilitarUsuario(usuario.id);
+         res.send({ message: 'Profesional y Usuario habilitado con exito' });
+      } else {
+         res.status(500).json({ message: 'Error al habilitar el profesional' });
+      }
+   }
+
+   static async deshabilitarProfesional(req, res) { // 🟢
+      const { id } = req.body;
+      const deshabilitado = await ProfesionalModel.deshabilitarProfesional(id);
+      if (deshabilitado) {
+         const profesional = await ProfesionalModel.obtenerProfesionalPorId(id);
+         const usuario = await UsuarioModel.obtenerUsuarioPorId(profesional.id_usuario);
+         await UsuarioModel.deshabilitarUsuario(usuario.id);
+         res.send({ message: 'Profesional y Usuario deshabilitado con exito' });
+      } else {
+         res.status(500).json({ message: 'Error al deshabilitar el profesional' });
+      }
+   }
+
+   static async agregarProfesion(req, res) { // 🟢
+      const { nombre } = req.body;
+      const idProfesion = await ProfesionalModel.agregarProfesion(nombre);
+      if (idProfesion) {
+         res.send({ message: 'Profesion agregada exitosamente', profesion: { id: idProfesion, nombre: nombre } });
+      } else {
+         res.status(500).json({ message: 'Error al registrar la profesion' });
+      }
+   }
+
+   static async agregarEspecialidad(req, res) { // 🟢
+      const { nombre } = req.body;
+      const idEspecialidad = await ProfesionalModel.agregarEspecialidad(nombre);
+      if (idEspecialidad) {
+         res.send({ message: 'Especialidad agregada exitosamente', especialidad: { id: idEspecialidad, nombre: nombre } });
+      } else {
+         res.status(500).json({ message: 'Error al registrar la especialidad' });
       }
    }
 
--- models/profesional.js
+++ models/profesional.js
@@ -2,72 +2,97 @@ import connection from "./db.js";
 
 export class ProfesionalModel {
 
-   static async obtenerProfesionalPorId(id) {
+   static async agregarProfesional(nombre, apellido, documento, domicilio, matricula, refeps, estado, idProfesion, idUsuario) { // 🟢
+      const [result] = await connection.execute(
+         'INSERT INTO profesional (nombre, apellido, documento, domicilio, matricula, refeps, estado, id_profesion, id_usuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+         [nombre, apellido, documento, domicilio, matricula, refeps, estado, idProfesion, idUsuario]
+      );
+      return result.insertId;
+   }
+
+   static async agregarProfesionalEspecialidad(idProfesional, idEspecialidad) { // 🟢
+      const [result] = await connection.execute(`
+         INSERT INTO profesional_especialidad(
+            id_profesional, 
+            id_especialidad
+         )
+         VALUES(?, ?)`,
+         [idProfesional, idEspecialidad]
+      );
+      return result.insertId;
+   }
+
+   static async obtenerEspecialidadesPorProfesional(id) { // 🟢
+      const [especialidades] = await connection.execute('SELECT * FROM profesional_especialidad WHERE id_profesional = ?', [id]);
+      return especialidades;
+   }
+   
+   static async obtenerProfesionalPorId(id) { // 🟢
       const [profesional] = await connection.execute(`SELECT * FROM profesional WHERE id = ?`, [id]);
       return profesional[0];
    }
    
    static async obtenerProfesionales() { // 🟢
-      const [profesionales] = await connection.execute(`
-         SELECT
-            p.id,
-            p.nombre,
-            p.apellido,
-            p.documento,
-            p.domicilio,
-            p.matricula,
-            p.refeps,
-            p.estado,
-            especialidad.nombre AS especialidad,
-            profesion.nombre AS profesion
-         FROM
-            profesional p
-         JOIN especialidad ON p.id_especialidad = especialidad.id
-         JOIN profesion ON especialidad.id_profesion = profesion.id
-         WHERE
-            p.estado = 1
-      `);
+      const [profesionales] = await connection.execute(`SELECT * FROM profesional`);
       return profesionales;
    }
-
+         
    static async obtenerProfesionalPorIdUsuario(id) { // 🟢
       const [profesional] = await connection.execute('SELECT * FROM profesional WHERE id_usuario = ?', [id]);
       return profesional[0];
    }
-
-   static async obtenerEspecialidadesPorProfesion(idProfesion) { // 🟢
-      const [especialidades] = await connection.execute('SELECT * FROM especialidad WHERE id_profesion = ?', [idProfesion]);
-      return especialidades;
-   }
-
+   
    static async obtenerProfesiones() { // 🟢
-      const [profesiones] = await connection.execute('SELECT * FROM profesion');
+      const [profesiones] = await connection.execute('SELECT * FROM profesion ORDER BY nombre;');
       return profesiones;
    }
 
-   static async agregarProfesional(nombre, apellido, documento, domicilio, matricula, refeps, estado, idEspecialidad, idUsuario) { // 🟢
-      const [result] = await connection.execute(
-         'INSERT INTO profesional (nombre, apellido, documento, domicilio, matricula, refeps, estado, id_especialidad, id_usuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-         [nombre, apellido, documento, domicilio, matricula, refeps, estado, idEspecialidad, idUsuario]
-      );
-      return result.insertId;
+   static async obtenerProfesionPorId(id) { // 🟢
+      const [profesion] = await connection.execute('SELECT * FROM profesion WHERE id = ?', [id]);
+      return profesion[0];
    }
-
-   static async obtenerEspecialidades() {
-      const [especialidades] = await connection.execute('SELECT * FROM especialidad');
+   
+   static async obtenerEspecialidades() { // 🟢
+      const [especialidades] = await connection.execute('SELECT * FROM especialidad ORDER BY nombre;');
       return especialidades;
    }
 
-   static async habilitarProfesional(id) {
-      const [result] = await connection.execute('UPDATE profesional SET estado = 1 WHERE id = ?', [id]);
+   static async obtenerEspecialidadPorId(id) { // 🟢
+      const [especialidad] = await connection.execute('SELECT * FROM especialidad WHERE id = ?', [id]);
+      return especialidad[0];
+   }
+
+   static async editarProfesional(id, nombre, apellido, documento, domicilio, matricula, refeps, idProfesion) { // 🟢
+      const [result] = await connection.execute(
+         'UPDATE profesional SET nombre = ?, apellido = ?, documento = ?, domicilio = ?, matricula = ?, refeps = ?, id_profesion = ? WHERE id = ?',
+         [nombre, apellido, documento, domicilio, matricula, refeps, idProfesion, id]
+      );
       return result.affectedRows;
    }
 
-   static async deshabilitarProfesional(id) {
+   static async eliminarProfesionalEspecialidad(id) { // 🟢
+      const [result] = await connection.execute('DELETE FROM profesional_especialidad WHERE id_profesional = ?', [id]);
+      return result.affectedRows;
+   }
+   
+   static async habilitarProfesional(id) { // 🟢
+      const [result] = await connection.execute('UPDATE profesional SET estado = 1 WHERE id = ?', [id]);
+      return result.affectedRows;
+   }
+   
+   static async deshabilitarProfesional(id) { // 🟢
       const [result] = await connection.execute('UPDATE profesional SET estado = 0 WHERE id = ?', [id]);
       return result.affectedRows;
    }
 
+   static async agregarProfesion(nombre) { // 🟢
+      const [result] = await connection.execute('INSERT INTO profesion (nombre) VALUES (?)', [nombre]);
+      return result.insertId;
+   }
 
-
+   static async agregarEspecialidad(nombre) { // 🟢
+      const [result] = await connection.execute('INSERT INTO especialidad (nombre) VALUES (?)', [nombre]);
+      return result.insertId;
+   }
+   
 }
