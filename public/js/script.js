3cc4f839df35b79496f45c1f8ccddeee97cfc1f5
kevinhuanca <kevinhuanca75@gmail.com>
2024-08-01 14:04:23 -0300

    Add password change functions

--- public/js/script.js
+++ public/js/script.js
@@ -45,4 +45,94 @@ document.addEventListener('DOMContentLoaded', () => {
    new DataTable('#tablaProfesionales', options);
    new DataTable('#tablaMedicamentos', options);
 
+   /**
+    * Script para cambiar contraseña
+    */
+
+   const modalClave = new bootstrap.Modal('#modalClave');
+   const modalMensaje = new bootstrap.Modal('#modalMensaje');
+
+   const cambiarBtn = document.querySelector('#cambiar');
+   cambiarBtn.addEventListener('click', () => {
+      modalClave.show();
+      document.querySelector('#mensajeError').innerText = '';
+      document.querySelector('#actualClave').value = '';
+      document.querySelector('#nuevaClave').value = '';
+      document.querySelector('#confirmarClave').value = '';
+      validarInputsClave();
+   })
+
+   const cambiarClaveBtn = document.querySelector('#cambiarClave');
+   cambiarClaveBtn.addEventListener('click', () => {
+      const mensajeError = document.querySelector('#mensajeError');
+      const actual = document.querySelector('#actualClave').value;
+      const nueva = document.querySelector('#nuevaClave').value;
+      const confirmar = document.querySelector('#confirmarClave').value;
+
+      if (nueva === confirmar) {
+         fetch('acceso/cambiarclave', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ actual, nueva })
+         })
+            .then((response) => response.json())
+            .then((data) => {
+               if (data.message) {
+                  modalMensaje.show();
+                  modalClave.toggle();
+               } else {
+                  mensajeError.innerText = 'Clave incorrecta';
+               }
+            })
+      } else {
+         mensajeError.innerText = 'Las claves no coinciden';
+      }
+   })
+
+   document.querySelector('#actualClave').addEventListener('input', validarInputsClave);
+   document.querySelector('#nuevaClave').addEventListener('input', validarInputsClave);
+   document.querySelector('#confirmarClave').addEventListener('input', validarInputsClave);
+
+   /**
+    * Script para mostrar datos
+    */
+
+   const modalDatos = new bootstrap.Modal('#modalDatos');
+   const modalBodyDatos = document.querySelector('#modalBodyDatos');
+
+   const datos = document.querySelector('#datos');
+   if (datos) {
+      datos.addEventListener('click', () => {
+         modalDatos.show();
+         let id = modalBodyDatos.getAttribute('data-id');
+
+         fetch(`/profesionales/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+               modalBodyDatos.innerHTML = '';
+               modalBodyDatos.innerHTML = `
+                  <b>DOCUMENTO:</b> ${data.documento} <br>
+                  <b>APELLIDO Y NOMBRE:</b> ${data.apellido}, ${data.nombre} <br>
+                  <b>DOMICILIO:</b> ${data.domicilio} <br>
+                  <b>MATRICULA:</b> ${data.matricula} <br>
+                  <b>REFEPS:</b> ${data.refeps} <br>
+                  <b>PROFESION:</b> ${data.profesion.nombre} <br>
+                  <b>ESPECIALIDAD:</b> ${data.especialidades ? data.especialidades.map(especialidad => especialidad.nombre).join(", ") : "No tiene"}
+               `
+            })
+      })
+   }
+   
 })
+
+function validarInputsClave() {
+   const actual = document.querySelector('#actualClave').value;
+   const nueva = document.querySelector('#nuevaClave').value;
+   const confirmar = document.querySelector('#confirmarClave').value;
+
+   if (actual === '' || nueva === '' || confirmar === '') {
+      document.querySelector('#cambiarClave').disabled = true;
+   } else {
+      document.querySelector('#cambiarClave').disabled = false;
+   }
+}
